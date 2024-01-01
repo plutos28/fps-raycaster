5ba46763e5da88d35058b6e88a17d0c3d4a00e21
plutos28 <gamevictor28@gmail.com>
2024-01-01 15:39:49 +0300

    draw the map

--- fps-raycaster/fps-raycaster.cpp
+++ fps-raycaster/fps-raycaster.cpp
@@ -27,11 +27,46 @@ void drop_ppm_image(const std::string filename, const std::vector<uint32_t>& ima
     ofs.close();
 }
 
+void draw_rectangle(std::vector<uint32_t>& img, const size_t img_w, const size_t img_h, const size_t x, const size_t y,
+    const size_t w, const size_t h, const uint32_t color) {
+    assert(img.size() == img_w * img_h);
+    for (size_t i = 0; i < w; i++) {
+        for (size_t j = 0; j < h; j++) {
+            size_t cx = x + i;
+            size_t cy = y + j;
+            assert(cx < img_w && cy < img_h);
+            img[cx + cy * img_w] = color;
+        }
+    }
+}
+
 int main()
 {
     const size_t win_w = 512;
     const size_t win_h = 512;
     std::vector<uint32_t> framebuffer(win_w * win_h, 255); // create vector with all the pixels in win_w*win_h and give it color value of 255(white)
+
+    const size_t map_w = 16;
+    const size_t map_h = 16;
+
+    const char map[] = "0000222222220000"\
+                       "1              0"\
+                       "1      11111   0"\
+                       "1     0        0"\
+                       "0     0  1110000"\
+                       "0     3        0"\
+                       "0   10000      0"\
+                       "0   0   11100  0"\
+                       "0   0   0      0"\
+                       "0   0   1  00000"\
+                       "0       1      0"\
+                       "2       1      0"\
+                       "0       0      0"\
+                       "0 0000000      0"\
+                       "0              0"\
+                       "0002222222200000";
+
+    assert(sizeof(map) == map_w * map_h + 1);
 
     // fill screen with color gradients
     for (size_t j = 0; j < win_h; j++) { // go through each row
@@ -40,6 +75,20 @@ int main()
             uint8_t g = 255 * i / float(win_w);
             uint8_t b = 0;
             framebuffer[i + j * win_w] = pack_color(r, g, b); // set the specific pixel to the combination of r, g, and b
+        }
+    }
+
+    // draw out the map
+    const size_t rect_w = win_w / map_w;
+    const size_t rect_h = win_h / map_h;
+
+    for (size_t j = 0; j < map_h; j++) {
+        for (size_t i = 0; i < map_w; i++) {
+            if (map[i + j * map_w] == ' ') continue; // don't draw anything on empty spaces
+            size_t rect_x = i * rect_w;
+            size_t rect_y = j * rect_h;
+            draw_rectangle(framebuffer, win_w, win_h, rect_x, rect_y, rect_w, rect_h, pack_color
+            (0, 255, 255));
         }
     }
 
